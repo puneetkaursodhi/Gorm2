21d7086f587929db469055b1ce4623bc442d0efa
deepak kr mittal <deepak.krmittal@intelligrape.com>
2015-02-24 10:22:09 +0530

    application version upgraded

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -2,6 +2,18 @@ dataSource {
     pooled = true
     driverClassName = "com.mysql.jdbc.Driver"
     dialect = org.hibernate.dialect.MySQL5InnoDBDialect
+    username = "root"
+    password = ""
+    properties {
+        maxActive = 40
+        initialSize = 20
+        minIdle = 5
+        numTestsPerEvictionRun = 3
+        testWhileIdle = true
+        validationQuery = "SELECT 1"
+        minEvictableIdleTimeMillis = (1000 * 60 * 5)
+        timeBetweenEvictionRunsMillis = (1000 * 60 * 5)
+    }
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -23,7 +35,8 @@ environments {
             username = "root"
             password = "igdefault"
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:mysql://localhost:3306/gorm2"
+            url = "jdbc:mysql://localhost:3306/gorm2?autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8"
+            logSql = true
         }
     }
     production {
